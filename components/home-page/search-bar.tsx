2d84c8417d4065a4f0b091a4e70d0937926b6738
Tam <tnhubh@gmail.com>
2024-04-04 00:10:53 +0700

    feat/search-bar_01:create searching bar

--- components/home-page/search-bar.tsx
+++ components/home-page/search-bar.tsx
@@ -8,16 +8,11 @@ import { BiSearch } from "react-icons/bi";
 
 const SearchBar = () => {
   const [query, setQuery] = useState<string>('');
-  // const [debouncedQuery, setDebouncedQuery] = useState('');
+  const [selectedResult, setSelectedResult] = useState<string>(''); // State để lưu kết quả đã chọn
   const router = useRouter();
 
-  // const [data, setData] = useState([]);
-
-  // Sử dụng useSWR để fetch dữ liệu
   const { data, error } = useSWR(
-    // Sử dụng debouncedQuery thay vì query
     query ? `${process.env.NEXT_PUBLIC_SERVER_URL}/get-key-search/${encodeURIComponent(query)}` : null,
-    // async callback function
     async (url: string) => {
       try {
         const response = await fetch(url);
@@ -42,42 +37,18 @@ const SearchBar = () => {
     setQuery(e.target.value);
   }
 
-  // useEffect(() => {
-  //   const timerId = setTimeout(() => {
-  //     setDebouncedQuery(query);
-  //   }, 500);
-
-  //   return () => clearTimeout(timerId);
-  // }, [query]);
-
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     if (debouncedQuery) {
-  //       try {
-  //         // api
-  //         // console.log(`Gọi API với query: ${process.env.NEXT_PUBLIC_SERVER_URL}/get-key-search/${encodeURIComponent(debouncedQuery)}`);
-  //         const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/get-key-search/${encodeURIComponent(debouncedQuery)}`);
-  //         const data = await response.json();
-  //         console.log(data.courseSearch); // Xử lý dữ liệu nhận được
-  //         setData(data.courseSearch)
-
-  //      } catch (error) {
-  //         console.error('Lỗi khi gọi API:', error);
-
-  //       }
-  //     }
-  //   };
-
-  //   fetchData();
-  // }, [debouncedQuery]); 
+  // Function để xử lý việc chọn một kết quả từ danh sách
+  const handleResultSelection = (result: string) => {
+    setSelectedResult(result);
+    setQuery(result); // Cập nhật nội dung của input với kết quả đã chọn
+  };
 
   return (
     <>
       <form className="flex items-center h-[50px] mt-3" onSubmit={searchHandler}>
         <input
           type="search"
-          placeholder="Search Courses..."
+          placeholder={selectedResult || "Search Courses..."} // Sử dụng kết quả đã chọn hoặc placeholder mặc định
           className="bg-transparent border dark:border-none dark:bg-[#575757] dark:placeholder:text-[#ffffffdd] rounded-l-[5px] px-2 h-full flex-1 outline-none font-josefin"
           value={query}
           onChange={handleChange}
@@ -87,24 +58,11 @@ const SearchBar = () => {
         </button>
       </form>
 
-      {/* {data.length > 0 ?  (
-        <ul className="relative z-10 bg-white shadow-md max-h-60 overflow-auto text-black">
-          {data.length > 0 ? (
-            data.map((item: { title: string }, index: number) => (
-              <li key={index} className="p-2 hover:bg-gray-100 cursor-pointer">
-                {item.title}
-              </li>
-            ))
-          ) : (
-            <li className="p-2 text-gray-500">No results found.</li>
-          )}
-        </ul>
-      ): <></>} */}
       {data ? (
         <ul className="relative z-10 bg-white shadow-md max-h-60 overflow-auto text-black">
           {data.length > 0 ? (
-            data.map(( name: string , index: number) => (
-              <li key={index} className="p-2 hover:bg-gray-100 cursor-pointer">
+            data.map((name: string, index: number) => (
+              <li key={index} className="p-2 hover:bg-gray-100 cursor-pointer" onClick={() => handleResultSelection(name)}>
                 {name}
               </li>
             ))
@@ -112,11 +70,9 @@ const SearchBar = () => {
             <li className="p-2 text-gray-500">No results found.</li>
           )}
         </ul>
-      ) : (<></>) }
-
+      ) : (<></>)}
     </>
   );
 };
 
 export default SearchBar;
-
