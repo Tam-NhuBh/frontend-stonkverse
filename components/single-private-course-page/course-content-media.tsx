22703735031ef0874b79a3842510091796ee378c
Tam <tnhubh@gmail.com>
2024-06-25 00:07:53 +0700

    Update tick quiz completion in course-lecture-list

--- components/single-private-course-page/course-content-media.tsx
+++ components/single-private-course-page/course-content-media.tsx
@@ -8,6 +8,7 @@ import LectureTabContent from "./lecture-tab-content";
 import { ICourseData, IQuestionQuiz } from "@/types";
 import toast from "react-hot-toast";
 import { getAnswersQuiz } from "@/lib/fetch-data";
+import { AiOutlineReload } from "react-icons/ai";
 
 interface Props {
   courseId: string;
@@ -33,173 +34,196 @@ const CourseContentMedia: FC<Props> = ({
   const [nextVideoTriggered, setNextVideoTriggered] = useState(false);
   const [quizSubmitted, setQuizSubmitted] = useState(false);
   const [currentQuizId, setCurrentQuizId] = useState<string>("");
-  const [isFirstLoading, setIsFirstLoading] = useState(false);
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [activeVideo]);
-
-  useEffect(() => {
-    setCurrentVideoHasQuiz(courseData[activeVideo]?.quiz?.length > 0);
-  }, [activeVideo, courseData]);
-
-  useEffect(() => {
-    if (!isFirstLoading) {
-      const fetchQuizCompletionStatus = async () => {
-        try {
-          const answers = await getAnswersQuiz(courseData?.[activeVideo]?._id.toString());
-          if (answers && answers.answers && answers.answers[courseData?.[activeVideo]?._id.toString()]) {
-            setQuizCompleted((prevCompleted) => {
-              const newCompleted = [...prevCompleted];
-              newCompleted[activeVideo] = true;
-              return newCompleted;
-            });
-          }
-        } catch (error) {
-          console.error("Error fetching quiz answers:", error);
-          toast.error("An error occurred while fetching quiz answers");
-        }
-      };
-
-      fetchQuizCompletionStatus();
-    }
-  }, [activeVideo, courseData]);
+  const [retakeQuizClicked, setRetakeQuizClicked] = useState(false);
+  const [videoQuizCompleted, setVideoQuizCompleted] = useState<boolean[]>(Array(courseData?.length).fill(false)); // State để lưu trạng thái hoàn thành quiz của từng video
 
   const filteredQuestions = (courseData?.[activeVideo]?.quiz ?? [])
-    .filter((question: IQuestionQuiz) => question.title !== undefined)
-    .map((question: IQuestionQuiz) => ({
-      id: question._id.toString(),
-      title: question.title as string,
-      mockAnswer: question.mockAnswer,
-      correctAnswer: question.correctAnswer ?? [],
-      maxScore: question.maxScore,
-    }));
+  .filter((question: IQuestionQuiz) => question.title !== undefined)
+  .map((question: IQuestionQuiz) => ({
+    id: question._id.toString(),
+    title: question.title as string,
+    mockAnswer: question.mockAnswer,
+    correctAnswer: question.correctAnswer ?? [],
+    maxScore: question.maxScore,
+  }));
 
-  const handleNextVideo = async () => {
-    if (currentVideoHasQuiz && !quizCompleted[activeVideo]) {
-      const quizId = courseData?.[activeVideo]?.quiz[0]?._id.toString();
-      if (quizId) {
-        setCurrentQuizId(quizId);
-        setShowQuizModal(true);
-        setNextVideoTriggered(true);
+useEffect(() => {
+  window.scrollTo(0, 0);
+}, [activeVideo]);
+
+useEffect(() => {
+  setCurrentVideoHasQuiz(courseData[activeVideo]?.quiz?.length > 0);
+}, [activeVideo, courseData]);
+
+useEffect(() => {
+  const fetchQuizCompletionStatus = async () => {
+    try {
+      const answers = await getAnswersQuiz(courseData?.[activeVideo]?._id.toString());
+      if (answers && answers.answers && answers.answers[courseData?.[activeVideo]?._id.toString()]) {
+        setQuizCompleted((prevCompleted) => {
+          const newCompleted = [...prevCompleted];
+          newCompleted[activeVideo] = true;
+          return newCompleted;
+        });
       }
-    } else {
-      setActiveVideo((prevIndex) => Math.min(prevIndex + 1, courseData.length - 1));
+    } catch (error) {
+      console.error("Error fetching quiz answers:", error);
+      toast.error("An error occurred while fetching quiz answers.");
     }
   };
 
-  const handleBackVideo = () => {
-    if (activeVideo > 0) {
-      setActiveVideo((prevIndex) => Math.max(prevIndex - 1, 0));
-    }
-  };
+  fetchQuizCompletionStatus();
+}, [activeVideo, courseData]);
 
-  const handleQuizSubmit = () => {
-    setQuizCompleted((prevCompleted) => {
-      const updatedCompleted = [...prevCompleted];
-      updatedCompleted[activeVideo] = true;
-      return updatedCompleted;
+const handleNextVideo = async () => {
+  if (currentVideoHasQuiz && !quizCompleted[activeVideo]) {
+    const quizId = courseData?.[activeVideo]?.quiz[0]?._id.toString();
+    if (quizId) {
+      setCurrentQuizId(quizId);
+      setShowQuizModal(true);
+      setNextVideoTriggered(true);
+    }
+  } else {
+    setActiveVideo((prevIndex) => Math.min(prevIndex + 1, courseData.length - 1));
+  }
+};
+
+const handleBackVideo = () => {
+  if (activeVideo > 0) {
+    setActiveVideo((prevIndex) => Math.max(prevIndex - 1, 0));
+  }
+};
+
+const handleQuizSubmit = () => {
+  setQuizCompleted((prevCompleted) => {
+    const updatedCompleted = [...prevCompleted];
+    updatedCompleted[activeVideo] = true;
+    return updatedCompleted;
+  });
+  setQuizSubmitted(true);
+  setShowQuizModal(false);
+  if (nextVideoTriggered) {
+    setActiveVideo((prevIndex) => Math.min(prevIndex + 1, courseData.length - 1));
+    setNextVideoTriggered(false);
+  }
+};
+
+const handleVideoClick = (videoIndex: number | ((prevIndex: number) => number)) => {
+  if (typeof videoIndex === 'number') {
+    const allPreviousQuizzesCompleted = courseData.slice(0, videoIndex).every((video, index) => {
+      return !video.quiz?.length || quizCompleted[index];
     });
-    setQuizSubmitted(true);
-    setShowQuizModal(false);
-    if (nextVideoTriggered) {
-      setActiveVideo((prevIndex) => Math.min(prevIndex + 1, courseData.length - 1));
-      setNextVideoTriggered(false);
-    }
-  };
 
-  const handleVideoClick = (videoIndex: number | ((prevIndex: number) => number)) => {
-    if (typeof videoIndex === 'number') {
-      if (quizCompleted[videoIndex] || videoIndex === activeVideo) {
-        setActiveVideo(videoIndex);
-      } else {
-        toast.error("Please complete the current quiz before moving to another video");
-      }
-    } else {
+    if (allPreviousQuizzesCompleted) {
       setActiveVideo(videoIndex);
+    } else {
+      toast.error("Please complete the current quiz before moving to another video.");
     }
-  };
+  } else {
+    setActiveVideo(videoIndex);
+  }
+};
 
-  return (
-    <div className="mt-[1px]">
-      {!showQuizModal && (
-        <>
+const handleRetakeQuiz = () => {
+  const quizId = courseData?.[activeVideo]?.quiz[0]?._id.toString();
+  if (quizId) {
+    setCurrentQuizId(quizId);
+    setShowQuizModal(true);
+    setRetakeQuizClicked(true);
+  }
+};
+
+return (
+  <div className="mt-[1px]">
+    {!showQuizModal && (
+      <>
+        <div className={`${openSidebar ? "w-[75%]" : "w-full"} transition-width max-[1100px]:w-full`}>
+          {activeContentType === "video" && (
+            <CoursePlayer
+              title={courseData?.[activeVideo]?.title}
+              videoUrl={courseData?.[activeVideo]?.videoUrl}
+            />
+          )}
+        </div>
+        <div className="container">
           <div className={`${openSidebar ? "w-[75%]" : "w-full"} transition-width max-[1100px]:w-full`}>
-            {activeContentType === "video" && (
-              <CoursePlayer
-                title={courseData?.[activeVideo]?.title}
-                videoUrl={courseData?.[activeVideo]?.videoUrl}
-              />
-            )}
-          </div>
-          <div className="container">
-            <div className={`${openSidebar ? "w-[75%]" : "w-full"} transition-width max-[1100px]:w-full`}>
-              <CourseLectureNavigator
-                onlyNext={activeVideo === 0}
-                onlyPrev={activeVideo === courseData.length - 1}
-                backHandler={handleBackVideo}
-                nextHandler={handleNextVideo}
-              />
+            <CourseLectureNavigator
+              onlyNext={activeVideo === 0}
+              onlyPrev={activeVideo === courseData?.length - 1}
+              backHandler={handleBackVideo}
+              nextHandler={handleNextVideo}
+            />
 
-              <h1 className="text-2xl font-semibold dark:text-dark_text mb-4">
+            <div className="flex justify-between items-center mb-4">
+              <h1 className="text-2xl font-semibold dark:text-dark_text mb-0">
                 {courseData?.[activeVideo]?.title}
               </h1>
-
-              <LectureTabContent
-                courseId={courseId}
-                refetch={refetch}
-                courseData={courseData}
-                activeVideo={activeVideo}
-                setActiveVideo={handleVideoClick}
-                setActiveContentType={setActiveContentType}
-              />
+              {quizCompleted[activeVideo] && (
+                <AiOutlineReload
+                  size={30}
+                  className="text-blue-500 cursor-pointer hover:text-blue-700 transition"
+                  onClick={handleRetakeQuiz}
+                  title="Retake Quiz"
+                />
+              )}
             </div>
-          </div>
-        </>
-      )}
 
-      {showQuizModal && currentVideoHasQuiz && (
-        <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center z-50">
-          <div className="relative bg-white p-1 rounded-lg w-full max-w-2xl">
-            <button className="absolute top-2 right-2" onClick={() => setShowQuizModal(false)}>
-              Close
-            </button>
-            <CourseQuiz
+            <LectureTabContent
               courseId={courseId}
-              contentId={courseData?.[activeVideo]?._id.toString()}
-              questions={filteredQuestions}
-              onClose={() => setShowQuizModal(false)}
-              onQuizSubmit={handleQuizSubmit}
-              quizId={currentQuizId}
+              refetch={refetch}
+              courseData={courseData}
+              activeVideo={activeVideo}
+              setActiveVideo={handleVideoClick}
+              setActiveContentType={setActiveContentType}
             />
           </div>
         </div>
-      )}
+      </>
+    )}
 
-      {!showQuizModal && (
-        <div className={`w-[25%] fixed top-[62px] right-0 h-full z-50 bg-white dark:bg-slate-900 border-l dark:border-slate-700 transition ${openSidebar ? "translate-x-0 opacity-100" : "translate-x-full opacity-0"} max-[1100px]:hidden`}>
-          <CourseLectureList
-            courseData={courseData}
-            setOpenSidebar={setOpenSidebar}
-            setIconHover={setIconHover}
-            activeVideo={activeVideo}
-            setActiveVideo={handleVideoClick}
-            setActiveContentType={setActiveContentType}
+    {showQuizModal && currentVideoHasQuiz && (
+      <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center z-50">
+        <div className="relative bg-white p-1 rounded-lg w-full max-w-2xl">
+          <button className="absolute top-2 right-2" onClick={() => setShowQuizModal(false)}>
+            Close
+          </button>
+          <CourseQuiz
+            courseId={courseId}
+            contentId={courseData?.[activeVideo]?._id.toString()}
+            questions={filteredQuestions}
+            onClose={() => setShowQuizModal(false)}
+            onQuizSubmit={handleQuizSubmit}
+            quizId={currentQuizId}
           />
         </div>
-      )}
+      </div>
+    )}
 
-      {!openSidebar && (
-        <div className="fixed right-0 top-[30%] bg-slate-900 z-[60] text-white cursor-pointer py-1.5 rounded-l-full pl-1 pr-2 flex items-center space-x-2 max-[1100px]:hidden"
-          onClick={() => setOpenSidebar(true)}
-          onMouseEnter={() => setIconHover(true)}
-          onMouseLeave={() => setIconHover(false)}
-        >
-          <BiSolidChevronsLeft size={20} className={`${iconHover && "translate-x-2"} transition`} />
-        </div>
-      )}
-    </div>
-  );
+    {!showQuizModal && (
+      <div className={`w-[25%] fixed top-[62px] right-0 h-full z-50 bg-white dark:bg-slate-900 border-l dark:border-slate-700 transition ${openSidebar ? "translate-x-0 opacity-100" : "translate-x-full opacity-0"} max-[1100px]:hidden`}>
+        <CourseLectureList
+          courseData={courseData}
+          setOpenSidebar={setOpenSidebar}
+          setIconHover={setIconHover}
+          activeVideo={activeVideo}
+          setActiveVideo={handleVideoClick}
+          setActiveContentType={setActiveContentType}
+          quizCompleted={quizCompleted}
+        />
+      </div>
+    )}
+
+    {!openSidebar && (
+      <div className="fixed right-0 top-[30%] bg-slate-900 z-[60] text-white cursor-pointer py-1.5 rounded-l-full pl-1 pr-2 flex items-center space-x-2 max-[1100px]:hidden"
+        onClick={() => setOpenSidebar(true)}
+        onMouseEnter={() => setIconHover(true)}
+        onMouseLeave={() => setIconHover(false)}
+      >
+        <BiSolidChevronsLeft size={20} className={`${iconHover && "translate-x-2"} transition`} />
+      </div>
+    )}
+  </div>
+);
 };
 
 export default CourseContentMedia;
